70dfd5e84e20f8310949ed458ebcbc48c0e7ea67
Nikita Mehare <nikitamehare8@gmail.com>
2025-12-20 16:32:37 +0530

    Update MongoDB connection string

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+require("dotenv").config(); // <- load .env variables
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.DATABASE_URL;
-
-// Start server FIRST
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 // Connect to MongoDB
 mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB connected ✅");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-  });
+  .catch((err) => console.log("MongoDB connection error:", err));
